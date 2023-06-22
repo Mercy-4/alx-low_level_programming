7ba5307c0afbd1cd78206c48b8fbb129ca98473b
Mercy-4 <mercykinyunye47@gmail.com>
2023-06-22 06:17:58 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,23 @@ if (n <= 98)
 for (; n <= 98; n++)
 {
 if (n == 98)
-{	
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ",  n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
 printf("%d", n);
 printf("\n");
 break;
